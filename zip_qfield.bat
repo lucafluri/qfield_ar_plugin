1c01a163285eed747f6b7916cc0e49b53b917f83
Luca Fluri <luca.fluri@fhnw.ch>
2024-11-28 09:29:21 +0100

    zip fix

--- zip_qfield.bat
+++ zip_qfield.bat
@@ -8,6 +8,6 @@ REM Remove the existing zip file if it exists
 del /f %OUTPUT_ZIP%
 
 REM Create a new zip file from the source directory
-powershell -command "Compress-Archive -Path %SOURCE_DIR% -DestinationPath %OUTPUT_ZIP%"
+powershell -command "Compress-Archive -Path %SOURCE_DIR%\* -DestinationPath %OUTPUT_ZIP%"
 
 echo Zip operation completed successfully.
